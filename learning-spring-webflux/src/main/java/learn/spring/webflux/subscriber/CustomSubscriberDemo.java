41a171a69f03f9262388d8b34f5ab800fad6abc5
Josh Williams <dukeywilliams@gmail.com>
2025-01-13 10:31:02 +0530

    Testing with log

--- learning-spring-webflux/src/main/java/learn/spring/webflux/subscriber/CustomSubscriberDemo.java
+++ learning-spring-webflux/src/main/java/learn/spring/webflux/subscriber/CustomSubscriberDemo.java
@@ -1,11 +1,16 @@
 package learn.spring.webflux.subscriber;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
+
+import java.util.logging.Level;
 
 public class CustomSubscriberDemo {
 
     public static void main(String args[]) {
         Flux.just(10, 420, 329, 793, 323)
+                .log("", Level.SEVERE, SignalType.ON_ERROR)
                 .subscribe(new MyIntSubscriber());
     }
 
